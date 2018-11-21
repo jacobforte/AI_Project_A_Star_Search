389d7c00daf11e43a6a277d6e66d3ff318b772e5
Jacob Forte <jforte7@kent.edu>
2018-11-20 23:01:45 -0500

    Add comments to movement code
    Also made some changes to the readme.

--- 2D Maze Project/Assets/Scripts/PlayerMovement.cs
+++ 2D Maze Project/Assets/Scripts/PlayerMovement.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-    public float moveTime = 0.1f;   //Time it takes to move the player
-    public LayerMask blockingLayer; //This layer checks for collision
+    public float moveTime = 0.1f;       //Time it takes to move the player
+    public LayerMask blockingLayer;     //Layer used for collision detection
 
-    private BoxCollider2D boxCollider;
-    private Rigidbody2D rb2D;
+    private BoxCollider2D boxCollider;  //Necesary to collide with other objects, need to disable when raytracing
+    private Rigidbody2D rb2D;           //Used to move our object
     private float inverseMoveTime;
-    private bool isMoving;
+    private bool isMoving;              //Prevents input while character is moving
     
+    //Initalize game object data
 	void Start () {
         boxCollider = GetComponent<BoxCollider2D>();
         rb2D = GetComponent<Rigidbody2D>();
@@ -22,7 +23,7 @@ public class PlayerMovement : MonoBehaviour {
     {
         if (!isMoving)
         {
-            //Initalize the player's move direction
+            //Initalize the character's move direction
             int horizontal = 0;
             int vertical = 0;
 
@@ -46,13 +47,16 @@ public class PlayerMovement : MonoBehaviour {
     //Return true if the player can move
     protected bool Move(int xDir, int yDir, out RaycastHit2D hit)
     {
+        //Calculate start and end positions for our movement
         Vector2 start = transform.position;
         Vector2 end = start + new Vector2(xDir, yDir);
         
+        //Check if we will hit a wall
         boxCollider.enabled = false;
         hit = Physics2D.Linecast(start, end, blockingLayer);
         boxCollider.enabled = true;
         
+        //If we don't hit a wall, begin moving
         if (hit.transform == null)
         {
             StartCoroutine(SmoothMovement(end));
@@ -61,6 +65,7 @@ public class PlayerMovement : MonoBehaviour {
         return false;
     }
     
+    //This allows the character to appear with smoth movement, we are incramentally setting the character's position to a location closer to the end position
     protected IEnumerator SmoothMovement(Vector3 end)
     {
         isMoving = true;
@@ -68,29 +73,23 @@ public class PlayerMovement : MonoBehaviour {
         
         while (sqrRemainingDistance > float.Epsilon)
         {
+            //Sets the characer's new position
             Vector3 newPostion = Vector3.MoveTowards(rb2D.position, end, inverseMoveTime * Time.deltaTime);
-            
             rb2D.MovePosition(newPostion);
+
             sqrRemainingDistance = (transform.position - end).sqrMagnitude;
             yield return null;
         }
         isMoving = false;
     }
-
+    
     void AttemptMove(int xDir, int yDir)
     {
         RaycastHit2D hit;
         Move(xDir, yDir, out hit);
         if (hit.transform == null)
             return;
-        
-        //Wall hitComponent = hit.transform.GetComponent<Wall>();
-        //if (!canMove && hitComponent != null)
-        //    OnCantMove(hitComponent);
-    }
 
-    //void OnCantMove(T component)
-    //{
-    //
-    //}
+        //If we are unable to move put code here
+    }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AI_Project_A_Star_Search
-This is empty for the moment.
+To Test the current version:
+1. Download and install unity from https://store.unity.com/download?ref=personal
+2. Fetch the latest version from Github
+3. Open unity
+4. Click Open, open find .../AI_Project_A_Star_Search/2D_Maze_Project/
+5. Select or open 2D_Maze_Project/ and press Select Folder
+6. Click the play button at the top middle.
